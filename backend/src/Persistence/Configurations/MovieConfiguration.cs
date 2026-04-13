cc907a11ab130f5a463d9daeac45c490233bdd90
Miclell <hem123v@gmail.com>
2026-04-13 22:45:51 +0300

    feat: add configuration

---
+++ backend/src/Persistence/Configurations/MovieConfiguration.cs
@@ -0,0 +1,44 @@
+using Core.Entities;
+using Core.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+internal sealed class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.ToTable("Movies");
+
+        builder.HasKey(movie => movie.Id);
+
+        builder.Property(movie => movie.Title)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(movie => movie.Status);
+
+        builder.Property(movie => movie.Year);
+
+        builder.Property(movie => movie.Genres)
+            .HasColumnType("text[]")
+            .IsRequired();
+
+        builder.Property(movie => movie.Notes)
+            .HasMaxLength(4096)
+            .IsRequired();
+
+        builder.Property(movie => movie.Rating)
+            .HasConversion(
+                rating => rating.HasValue ? rating.Value.Value : (decimal?)null,
+                value => value.HasValue ? new Rating(value.Value) : null)
+            .HasPrecision(3, 1);
+
+        builder.Property(movie => movie.WatchedDate);
+
+        builder.Property(movie => movie.CreatedAt);
+
+        builder.Property(movie => movie.UpdatedAt);
+    }
+}
